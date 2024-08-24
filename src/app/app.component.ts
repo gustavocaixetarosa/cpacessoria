a546aff6b988599d7b51a0ee168da1b0e7fdcee5
Gustavo <gstvcaixeta@gmail.com>
2024-08-24 10:37:00 -0300

    Criacão header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ServicosComponent } from './components/servicos/servicos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ServicosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
